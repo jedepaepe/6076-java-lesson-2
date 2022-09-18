3aa035e9b508082d6e1fa66bf9420e3ed519cf0e
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-09-18 11:37:42 +0200

    Initial commit

---
+++ src/BonjourVertical.java
@@ -0,0 +1,16 @@
+public class BonjourVertical {
+    public static void main(String[] args) {
+        System.out.println("B");
+        System.out.println("o");
+        System.out.println("n");
+        System.out.println("j");
+        System.out.println("o");
+        System.out.println("u");
+        System.out.println("r");
+        System.out.println("");
+        System.out.println("t");
+        System.out.println("o");
+        System.out.println("u");
+        System.out.println("s");
+    }
+}
