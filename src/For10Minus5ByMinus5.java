05d1ace07f8fb43cb7f0149ab8cdd03b4876247c
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-09-18 12:43:36 +0200

    ex for

---
+++ src/For10Minus5ByMinus5.java
@@ -0,0 +1,7 @@
+public class For10Minus5ByMinus5 {
+    public static void main(String[] args) {
+        for (int i = 10; i >= -5; i = i - 5) {
+            System.out.print(i + " ; ");
+        }
+    }
+}
