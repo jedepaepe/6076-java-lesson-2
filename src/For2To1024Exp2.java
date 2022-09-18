558a475b26ca28cedc17b8d63e0123076009e8b7
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-09-18 12:48:36 +0200

    ex for

---
+++ src/For2To1024Exp2.java
@@ -0,0 +1,7 @@
+public class For2To1024Exp2 {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 1024; i = i * 2) {
+            System.out.print(i + " -> ");
+        }
+    }
+}
