bc09e74a4ac53445ca41ce3d5ed083a88a908a5f
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-09-18 12:29:31 +0200

    ex for

---
+++ src/ForMinus1To1.java
@@ -0,0 +1,7 @@
+public class ForMinus1To1 {
+    public static void main(String[] args) {
+        for (int i = -1; i <= 1; ++i) {
+            System.out.println(i + " * 2 " + " = " + i * 2);
+        }
+    }
+}
