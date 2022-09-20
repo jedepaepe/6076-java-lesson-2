fd1d42909c0dab217ac30ff79be6e139a6ffddf3
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-09-20 10:59:48 +0200

    ex for

---
+++ src/SimpleFor10To20By2Other.java
@@ -0,0 +1,7 @@
+public class SimpleFor10To20By2Other {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; ++i) {
+            System.out.println(10 + i * 2);
+        }
+    }
+}
---
+++ src/SimpleFor2To1024Exp2WithMath.java
@@ -0,0 +1,7 @@
+public class SimpleFor2To1024Exp2WithMath {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; ++i) {
+            System.out.println(Math.pow(2, i));
+        }
+    }
+}
