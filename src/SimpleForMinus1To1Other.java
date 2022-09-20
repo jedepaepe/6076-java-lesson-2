83269cb3f2040cdccd112c834cdfc645f28fa2b7
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-09-20 11:04:19 +0200

    ex for

---
+++ src/SimpleForMinus1To1Other.java
@@ -0,0 +1,7 @@
+public class SimpleForMinus1To1Other {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; ++i) {
+            System.out.println(-1 + i);
+        }
+    }
+}
